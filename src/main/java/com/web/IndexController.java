1d8e9b0f1f992398de82cb1a1d0f994640ee95cf
e <834337625@qq.com>
2018-06-06 16:05:33 +0800

    miaoshu

--- src/main/java/com/web/IndexController.java
+++ src/main/java/com/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @GetMapping("/index")
     public String index(){
+
         System.out.println("index");
+
+        logger.info("ssss");
+        logger.error("ssss");
         return "index";
     }
     @GetMapping("/index2")
